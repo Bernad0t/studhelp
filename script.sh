dc87bf6b87b619b2fab70c38ccde0378be7cdb4a
Bernad0t <dnsvesna27092015@gmail.com>
2024-12-11 13:05:03 +0300

    change

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd frontend
-npm install
-cd ..
-cd python_files
-py -m venv venv
-pip install -r requirements.txt
-venv/scripts/activate
-uvicorn api.main:app --reload && cd .. && cd frontend && npm start
